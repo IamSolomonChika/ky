9939e42c47a5869f96598115358f69692eafaef6
Solomon Chika <solomon.chika1@gmail.com>
2023-03-11 11:52:27 -0500

    scroll animation

--- public/js/main.js
+++ public/js/main.js
@@ -11,11 +11,43 @@ cancelMenu.addEventListener('click', () => {
   menu.classList.add('hidden');
 });
 
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      document.querySelectorAll(".antn")[0].classList.add("fadeinbottom");
+    }
+  })
+});
 
-// function loadPage(pageName) {
-//     fetch(`/${pageName}`)
-//       .then(response => response.text())
-//       .then(html => document.querySelector('main').innerHTML = html)
-//       .catch(error => console.error(error));
-//   }
-  
+observer.observe(document.querySelector(".aim"))
+
+const observerr = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      document.querySelectorAll(".antnn")[0].classList.add("fadeinbottom");
+    }
+  })
+});
+
+observerr.observe(document.querySelector(".aimm"))
+
+
+const observer1 = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      document.querySelectorAll(".antn1")[0].classList.add("fadeinleft");
+    }
+  })
+});
+
+observer1.observe(document.querySelector(".aim1"))
+
+const observer2 = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      document.querySelectorAll(".antn2")[0].classList.add("fadeinbottom1");
+    }
+  })
+});
+
+observer2.observe(document.querySelector(".aim2"));
