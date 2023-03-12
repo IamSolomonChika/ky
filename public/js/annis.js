11c9a812db03786b783705ba0eaadf64ddc009a6
Solomon Chika <solomon.chika1@gmail.com>
2023-03-11 20:30:59 -0500

    about

---
+++ public/js/annis.js
@@ -0,0 +1,15 @@
+// Animation for the Home Page
+
+ScrollReveal({
+    reset: false,
+    distance: "1000px",
+    duration: 2500,
+  });
+
+  ScrollReveal().reveal(".anni", { delay: 0, origin: "right" });
+  ScrollReveal().reveal(".anni2", { delay: 0, origin: "bottom" });
+
+
+// Animation for the About Page
+ScrollReveal().reveal(".abb", { delay: 0, origin: "left"});
+ScrollReveal().reveal(".abbd", { delay: 0, origin: "right"});
