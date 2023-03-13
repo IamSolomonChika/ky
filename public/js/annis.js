ba4f55fedb629cfed3de5ae84b201268234aa758
Solomon Chika <solomon.chika1@gmail.com>
2023-03-13 09:24:52 -0400

    services

--- public/js/annis.js
+++ public/js/annis.js
@@ -2,14 +2,11 @@
 
 ScrollReveal({
     reset: false,
-    distance: "1000px",
-    duration: 2500,
+    distance: "100px",
+    duration: 4500,
   });
 
+  ScrollReveal().reveal(".abb", { delay: 0, origin: "left"});
+  ScrollReveal().reveal(".abb2", { delay: 0, origin: "top"});
   ScrollReveal().reveal(".anni", { delay: 0, origin: "right" });
   ScrollReveal().reveal(".anni2", { delay: 0, origin: "bottom" });
-
-
-// Animation for the About Page
-ScrollReveal().reveal(".abb", { delay: 0, origin: "left"});
-ScrollReveal().reveal(".abbd", { delay: 0, origin: "right"});
