971daa37392df5f4ca143918c78dc8e7ca461461
Solomon Chika <solomon.chika1@gmail.com>
2023-03-26 19:39:47 +0100

    SEO

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const expresslayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const session = require('express-session');
+const flash = require('connect-flash');
 const PORT = process.env.PORT || 4000;
+require('dotenv').config();
 
 const app = express();
 
@@ -9,11 +13,21 @@ app.use(expresslayouts);
 app.set('layout', 'layout/layout.ejs');
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/css'));
 app.use(express.static(__dirname + '/public/js'));
 app.use(express.static(__dirname + '/public/imgs'));
+app.use(session({
+    secret: 'secret',
+    cookie: { maxAge: 60000},
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(flash());
+
+
 
 app.get('/', (req, res, next) => {
     res.render('index', {text: 'This is Home'})
@@ -28,8 +42,42 @@ app.get('/services', (req, res, next) => {
 });
 
 app.get('/contact', (req, res, next) => {
-    res.render('contact', {text: 'This is contact'})
+    res.render('contact')
 });
+
+app.post('/send_mail', (req, res, next) => {
+
+    const email = req.body.email;
+    const reciever = process.env.RECIEVER;
+    const subject = `Message from ${req.body.name}: ${req.body.subject}`;
+    const message = req.body.message;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PASSWORD,
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: reciever,
+        subject,
+        html: `<h4>Sender's Name: ${req.body.name}</h4><h4> Sender's Email: ${email} </h4><h4> Subject: ${subject} </h4><h4>Message: ${message}</h4>`,
+    }
+
+    transporter.sendMail(mailOptions, function(error){
+        if (error) {
+            console.log(error);
+            res.redirect('/contact');
+        } else {
+            console.log("Mail sent successfully");
+            req.flash('message', 'Your message was sent successfully');
+            res.redirect('/contact');
+        }
+    })
+})
 
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT)
--- public/js/main.js
+++ public/js/main.js
@@ -11,45 +11,48 @@ cancelMenu.addEventListener('click', () => {
   menu.classList.add('hidden');
 });
 
+// Contact Form
+const contactForm = document.querySelector('#contact');
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let subject = document.getElementById('subject');
+let message = document.getElementById('message');
+let contactSubmitted = document.getElementById('sub-loading');
+let loading = document.getElementById('loading');
+let successdiv = document.getElementById('success-alert');
+let errordiv = document.getElementById('error-alert');
 
 
-// const observer = new IntersectionObserver(entries => {
-//   entries.forEach(entry => {
-//     if(entry.isIntersecting){
-//       document.querySelectorAll(".antn")[0].classList.add("fadeinbottom");
-//     }
-//   })
-// });
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  contactSubmitted.classList.add('hidden');
+  loading.classList.remove('hidden');
 
-// observer.observe(document.querySelector(".aim"))
-
-// const observerr = new IntersectionObserver(entries => {
-//   entries.forEach(entry => {
-//     if(entry.isIntersecting){
-//       document.querySelectorAll(".antnn")[0].classList.add("fadeinbottom");
-//     }
-//   })
-// });
-
-// observerr.observe(document.querySelector(".aimm"))
-
-
-// const observer1 = new IntersectionObserver(entries => {
-//   entries.forEach(entry => {
-//     if(entry.isIntersecting){
-//       document.querySelectorAll(".antn1")[0].classList.add("fadeinleft");
-//     }
-//   })
-// });
-
-// observer1.observe(document.querySelector(".aim1"))
-
-// const observer2 = new IntersectionObserver(entries => {
-//   entries.forEach(entry => {
-//     if(entry.isIntersecting){
-//       document.querySelectorAll(".antn2")[0].classList.add("fadeinbottom1");
-//     }
-//   })
-// });
-
-// observer2.observe(document.querySelector(".aim2"));
+  let formData = {
+    name: name.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value
+  }
+  
+  let data = new XMLHttpRequest();
+  data.open('POST', '/send_mail');
+  data.setRequestHeader('Content-Type', 'application/json');
+  data.onload = function(){
+    if (data.status === 200){
+      name.value = '';
+      email.value = '';
+      message.value = '';
+      subject.value = '';
+      loading.classList.add('hidden');
+      contactSubmitted.classList.remove('hidden');
+      successdiv.classList.remove('hidden');
+    } else {
+      loading.classList.add('hidden');
+      contactSubmitted.classList.remove('hidden');
+      errordiv.classList.remove('hidden');
+    }
+  }
+ 
+  data.send(JSON.stringify(formData));
+});
